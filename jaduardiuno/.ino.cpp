a9f7da1bd9f612fb3b538355d03c974a92b226a1
Thomas Pasch <thomas.pasch@gmx.de>
2017-04-23 07:53:45 +0200

    support for sloeber 4.0

---
+++ jaduardiuno/.ino.cpp
@@ -0,0 +1,18 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2017-04-23 07:49:28
+
+#include "Arduino.h"
+#include <Wire.h>
+#include <WireRtcLib.h>
+#include <SevenSegmentTM1637.h>
+void setup() ;
+void loop() ;
+void hmDisplay(uint8_t hours, uint8_t minutes) ;
+
+#include "jaduardiuno.ino"
+
+
+#endif
